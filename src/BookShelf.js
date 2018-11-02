61dbe6a66377c4ddc1453f69813a7c9eec039ceb
feezo <jimohafis@yahoo.co.uk>
2018-11-02 10:35:11 +0000

    added shelf component

---
+++ src/BookShelf.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class BookShelf extends React.Component {
+  render () {
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">Currently Reading</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            <li>
+
+            </li>
+          </ol>
+        </div>
+      </div>
+          )
+        }
+      }
+
+export default BookShelf
