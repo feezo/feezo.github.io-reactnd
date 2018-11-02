b5816f613db08d705180e7ee361e271ddda4d63e
feezo <jimohafis@yahoo.co.uk>
2018-11-02 10:33:56 +0000

    extracted components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react'
 // import * as BooksAPI from './BooksAPI'
+import BookShelf from './BookShelf'
 import SearchBar from './SearchBar'
 import MainPage from './MainPage'
+import Book from './Book'
 import './App.css'
 
 class BooksApp extends React.Component {
@@ -12,7 +14,7 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        <MainPage />
+        <BookShelf />
       </div>
     )
    }
