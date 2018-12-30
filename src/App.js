b3b2f59d2a805c02dc538aa4689393517e107137
feezo <jimohafis@yahoo.co.uk>
2018-12-30 08:13:08 +0000

    response to review

--- src/App.js
+++ src/App.js
@@ -61,69 +61,57 @@ updateQuery = (query) => {
     let matchingShelf = this.state.books.filter(book =>
       displayedBook.id === book.id
     )
-    return matchingShelf.length ? matchingShelf[0].shelf : undefined
+    return matchingShelf.length ? matchingShelf[0].shelf : "none"
   }
 
+  // updateSearchedBooks = (query) => {
+    //     query ? BooksAPI.search(query).then((searchedBooks) =>{
+      //       /*fetch searchedbooks using the method defined in BooksAPI */
+      //       // The response we get from BooksAPI.search is an array of books that don't have a shelf value,
+      //       // even if the book is in one of the shelves, so we should write a function
+      //       // to compare each book in searchedBooks with what we've got in state.books to retrieve the .shelf value,
+      //       // or assign shelf = 'none' if the book in not in any shelf.
+      //       // This is required to make books has the correct shelf option selected in the search page.
+      //       let book1 = {this.state.books};
+      //       function compareArrays(searchedBooks, book1){
+        //        return searchedBooks.every((e)=> book1.includes(e));
+        //       }
+        //
+        //
+        //
+        //       searchedBooks.error ? this.setState({searchedBooks:[]}):
+        //       this.setState({searchedBooks})
+        //     }) :
+        //     this.setState({searchedBooks : []})
+        //   }
+
+        render() {
+          return (
+            <div className="app">
+
+            <Route exact path="/" render={() =>
+              < MainPage
+              books = {this.state.books} //calling all my books
+              moveShelf = {this.moveShelf}
+
+              />
+            }/>
+
+            <Route path="/search" render={() =>
+              < SearchBar
+              searchedBooks = {this.state.searchedBooks}
+              books={this.state.books}
+              moveShelf = {this.moveShelf}
+              updateQuery={this.updateQuery}
+              updateSearchedBooks={this.updateSearchedBooks}
+              query={this.state.query}
+              />
+            }/>
 
 
+            </div>
+          )
+        }
+      }
 
-
-
-
-
-
-
-// updateSearchedBooks = (query) => {
-//     query ? BooksAPI.search(query).then((searchedBooks) =>{
-//       /*fetch searchedbooks using the method defined in BooksAPI */
-//       // The response we get from BooksAPI.search is an array of books that don't have a shelf value,
-//       // even if the book is in one of the shelves, so we should write a function
-//       // to compare each book in searchedBooks with what we've got in state.books to retrieve the .shelf value,
-//       // or assign shelf = 'none' if the book in not in any shelf.
-//       // This is required to make books has the correct shelf option selected in the search page.
-//       let book1 = {this.state.books};
-//       function compareArrays(searchedBooks, book1){
-//        return searchedBooks.every((e)=> book1.includes(e));
-//       }
-//
-//
-//
-//       searchedBooks.error ? this.setState({searchedBooks:[]}):
-//       this.setState({searchedBooks})
-//     }) :
-//     this.setState({searchedBooks : []})
-//   }
-
-
-
-
-
-  render() {
-    return (
-      <div className="app">
-
-      <Route exact path="/" render={() =>
-        < MainPage
-          books = {this.state.books} //calling all my books
-          moveShelf = {this.moveShelf}
-
-        />
-      }/>
-
-        <Route path="/search" render={() =>
-          < SearchBar
-          searchedBooks = {this.state.searchedBooks}
-          books={this.state.books}
-          moveShelf = {this.moveShelf}
-          updateQuery={this.updateQuery}
-          updateSearchedBooks={this.updateSearchedBooks}
-          />
-        }/>
-
-
-      </div>
-    )
-   }
-}
-
-export default BooksApp
+      export default BooksApp
--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Book from './Book'
-import PropTypes from 'prop-types';
+
 
 class BookShelf extends React.Component {
   render () {
